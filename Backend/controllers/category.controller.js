89d0fa34fbc59288bff4eb58e1057eec928afe8f
kotawalataha52 <kotawalataha52@gmail.com>
2026-05-29 10:39:51 +0530

    updating and deleting of categories

--- Backend/controllers/category.controller.js
+++ Backend/controllers/category.controller.js
@@ -45,4 +45,48 @@ const getCategories = asyncHandler(async (req,res) => {
     ))
 });
 
-export {createCategory,getCategories};
+const deleteCategory = asyncHandler(async (req,res)=>{
+    const {id} = req.params||{};
+    if(!id){
+        throw new ApiError(400,"Category id is required");
+    }
+    const category = await Category.findOne({
+        _id:id,
+        user:req.user._id
+    })
+    if(!category){
+        throw new ApiError(404,"Category not found");
+
+    }
+    const deletedCategory = await Category.deleteOne();
+    if(deletedCategory.deletedCount === 0){
+        throw new ApiError(500,"Something went wrong");
+    }
+    return res.status(200).json(new ApiResponse(200,{data:deletedCategory},"Category deleted successfully"));
+});
+
+const updateCategory = asyncHandler(async (req,res)=>{
+    const {id} = req.params || {};
+    const {name,type} = req.body||{};
+
+    if(!name || !type){
+        throw new ApiError(400,"Name and type are required");
+    }
+    const category = await Category.findOne({
+        _id:id,
+        user:req.user._id
+    })
+    if(!category){
+        throw new ApiError(404,"Category not found");
+    }
+    category.name = name;
+    category.type = type;
+
+    const updatedCategory = await category.save();
+    // if(!updatedCategory){
+    //     throw new ApiError(500,"Something went wrong");
+    // }  .SAVE METHOD EITHER RETURNS THE UPDATED DOCUMENT OR THROWS AN ERROR SO NO NEED TO MANUALLY CHECK FOR IT.
+    return res.status(200).json(new ApiResponse(200,{data:updatedCategory},"Category updated succcessfully"));
+})
+
+export {createCategory,getCategories,deleteCategory,updateCategory};
