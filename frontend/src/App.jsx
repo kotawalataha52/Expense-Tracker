de83c8ea74f591dad21e0a752af3b98629784ff9
kotawalataha52 <kotawalataha52@gmail.com>
2026-05-31 10:47:21 +0530

    frontend setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,24 @@
+
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={viteLogo} className="logo" alt="Vite logo" />
+        <img src={reactLogo} className="logo" alt="React logo" />
+        <p>Hello Vite!</p>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </header>
+    </div>
+  )
+}
+
+export default App
